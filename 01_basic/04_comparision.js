6bf36bc84f092061e1a6e66fa5eb56e2b87ae8f9
Harsh Jaiswal <154736568+Jaiswal-Harsh17@users.noreply.github.com>
2023-12-24 18:23:19 +0000

    Comparisions

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)
+// console.log("02">1)
+
+//Try to avoid this type of code 
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) 
+// Comparisons convert null to a number, treating it as 0. That's why null >= 0 is true and null > 0 is false.
+
+console.log("2"===2) // === is known as strict check is doesn't only checks the value but also checks the data type.
+
