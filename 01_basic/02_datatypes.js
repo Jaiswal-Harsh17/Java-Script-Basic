f4a32cc4f4be8d913bfdd8f55e32294a242ac14f
Harsh Jaiswal <154736568+Jaiswal-Harsh17@users.noreply.github.com>
2023-12-24 14:31:11 +0000

    Data Types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we can't us alert as we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Harsh");
+
+// Data Types
+let name = "Harsh"
+let age = 18
+let isLoggedIn = true
+
+// number=>2 to the power 53
+// bigint
+//string =>""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null)  // output=object
+console.log(typeof undefined)  // output=undefined
