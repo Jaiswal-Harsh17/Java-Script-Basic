ab9b915a48eca60bf9d8692e6ba143e380b3b17e
Harsh Jaiswal <154736568+Jaiswal-Harsh17@users.noreply.github.com>
2023-12-24 18:22:42 +0000

    Conversions

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,32 +1,32 @@
 let score = "23abc"
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
 
 let score1 = null
-console.log(typeof score1)
-console.log(typeof (score1))
+//console.log(typeof score1)
+//console.log(typeof (score1))
 let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1)
-console.log(valueInNumber1);
+//console.log(typeof valueInNumber1)
+//console.log(valueInNumber1);
 
 let score2 = undefined
-console.log(typeof score2)
-console.log(typeof (score2))
+//console.log(typeof score2)
+//console.log(typeof (score2))
 let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2);
+//console.log(typeof valueInNumber2)
+//console.log(valueInNumber2);
 
 
 let score3 = true
-console.log(typeof score3)
-console.log(typeof (score3))
+//console.log(typeof score3)
+//console.log(typeof (score3))
 let valueInNumber3 = Number(score3)
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3);
+//console.log(typeof valueInNumber3)
+//console.log(valueInNumber3);
 
 //"33" => 33
 // "33abs" => NaN
@@ -37,9 +37,32 @@ console.log(valueInNumber3);
 
 let isLoggedIn = 1
 let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn)
+//console.log(booleanLoggedIn)
 
-let someNum= 33
-let stringnum=String(someNum)
-console.log(typeof someNum)
-console.log(typeof stringnum)
+let someNum = 33
+let stringnum = String(someNum)
+//console.log(typeof someNum)
+//console.log(typeof stringnum)
+
+// ************** Operations ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(3-2)
+// console.log(3*3)
+// console.log(4/2)
+// console.log(2**3)
+// console.log(4%3)
+
+let str1 = "Hello "
+let str2 = "Harsh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
